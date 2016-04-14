6eea65ed8d33266aab2d2407a2e829a20fc36758
Dana Morton <dana.n.morton@gmail.com>
2016-04-14 16:50:14 -0700

    conditionals

--- introR1.R
+++ introR1.R
@@ -165,4 +165,79 @@ for (file in filenames){
   analyze(file)
 }
 
+#saving plots to a file
 
+
+analyze <- function(filename) {
+  # Plots the average, min, and max inflammation over time.
+  # Input is character string of a csv file.
+  dat <- read.csv(file = filename, header = FALSE)
+  avg_day_inflammation <- apply(dat, 2, mean)
+  plot(avg_day_inflammation)
+  max_day_inflammation <- apply(dat, 2, max)
+  plot(max_day_inflammation)
+  min_day_inflammation <- apply(dat, 2, min)
+  plot(min_day_inflammation)
+}
+
+
+analyze_all <- function(pattern) {
+  # Runs the function analyze for each file in the current working directory
+  # that contains the given pattern.
+  filenames <- list.files(path = "data", pattern = pattern, full.names = TRUE)
+  for (f in filenames) {
+    analyze(f)
+  }
+}
+
+
+#print plots to PDF
+pdf("inflammation-01.pdf")
+analyze("inflammation-01.csv")
+dev.off()  #this is important! "closes" the pdf. otherwise new analyses will overwrite the pdf
+
+pdf("inflammation-02.pdf")
+analyze("inflammation-02.csv")
+dev.off()
+
+
+#Conditionals - "if" "then" statements
+
+num <- 102
+if (num > 100){
+  print ("greater")  #need curly brackets for each part of the statement
+} else {
+  print ("not greater")
+}
+print("done")    #print done at end to show that the program is done running
+
+
+# a slightly more complicated conditional 
+sign <- function(num) {
+  if (num > 0) {    # can use, <==, >==, =/=
+    return(1)
+  }else if (num == 0) {    # is num exactly equal to 0? if so, return 0
+    return(0)
+  }else {
+    return(-1)
+  }
+}
+  
+sign (0)
+sign(10)
+sign(-1)
+
+
+# can use and's(&) and or's (|) to string together conditionals:
+
+if (1>0 & -1 > 0) {
+  print ("both parts are true")
+} else {
+  print ("at least one part is not true")
+}
+
+if (1>0 | -1 > 0) {
+  print ("at least one part is true")
+} else {
+  print ("niether part is true")
+}
